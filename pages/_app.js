d899923283bf628226132111eb24aee2cba842c5
Ashwin <ashwin.singh89@gmail.com>
2020-10-24 18:46:45 +0530

    integrated Shopify AppBridge

--- pages/_app.js
+++ pages/_app.js
@@ -3,18 +3,23 @@ import Head from 'next/head';
 import {AppProvider} from '@shopify/polaris';
 import '@shopify/polaris/styles.css';
 import translations from '@shopify/polaris/locales/en.json';
+import { Provider } from '@shopify/app-bridge-react';
+import Cookies from 'js-cookie';
 class MyApp extends App{
     render(){
-        const { Component, pageProps } = this.props;
+		const { Component, pageProps } = this.props;
+		const config = { apiKey: API_KEY, shopOrigin: Cookies.get("shopOrigin"), forceRedirect: true };
         return(
             <>
                 <Head>
                     <title>Sample App</title>
                     <meta charSet="utf-8"/>
                 </Head>
-                <AppProvider i18n={translations}>
-                    <Component {...pageProps} />
-                </AppProvider>
+				<Provider config={config}>
+					<AppProvider i18n={translations}>
+						<Component {...pageProps} />
+					</AppProvider>
+				</Provider>
             </>
         )
     }
