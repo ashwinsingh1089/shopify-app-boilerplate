aee6db1fe47420f14cfc959dde0047d83d7f6036
Ashwin <ashwin.singh89@gmail.com>
2020-10-24 12:37:55 +0530

    first commit

---
+++ pages/index.js
@@ -0,0 +1,19 @@
+import { TextStyle, Page, Layout, EmptyState } from '@shopify/polaris';
+const img = 'https://cdn.shopify.com/s/files/1/0757/9955/files/empty-state.svg';
+const Index = () => (
+    <Page>
+        <Layout>
+            <EmptyState
+                heading="Discount your products temporarily"
+                action={{
+                    content: "Select Products",
+                    onAction: () => console.log('clicked')
+                }}
+                image={img}
+            >
+                <p>Select products to change their price temporarily.</p>
+            </EmptyState>
+        </Layout>
+    </Page>
+);
+export default Index;
